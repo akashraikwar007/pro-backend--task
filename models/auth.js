bf9e8378b7d6a78c8f5b5327ef45d04b16650d91
akashraikwar007 <akashraikwar763@gmail.com>
2025-04-30 11:44:14 +0530

    first commit

---
+++ models/auth.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const authSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true, trim: true },
+  gender: { type: String, required: true },
+  
+});
+
+// Password hash middleware
+authSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Compare password method
+authSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("Auth", authSchema);
